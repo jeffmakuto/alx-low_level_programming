09b0aabdfd21de5fbc8317001cede0adf0dd9e89
jeffmakuto <hjeffmakuto@gmail.com>
2023-03-16 13:25:51 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,15 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Error)
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a newline, to standard error.
+ * Return: Always 1.
  */
+
+
 int main(void)
+
 {
-	const char *str1 = "and that piece of art is useful\"";
-	const char *str2 = " - Dora Korpar, ";
-	const char *str3 = "2015-10-19\n";
-
-	/* write to stdout using the write function */
-
-	write(STDOUT_FILENO, str1, 32);
-	write(STDOUT_FILENO, str2, 16);
-	write(STDOUT_FILENO, str3, 12);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
