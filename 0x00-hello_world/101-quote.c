f0af004a8c43d069435411617fcd88c55f508b27
jeffmakuto <hjeffmakuto@gmail.com>
2023-03-16 13:09:04 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
- * Return: Always 0 (Sucess)
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	const char *str1 = "and that piece of art is useful\"";
+	const char *str2 = " - Dora Korpar, ";
+	const char *str3 = "2015-10-19\n";
+
+	/* write to stdout using the write function */
+
+	write(STDOUT_FILENO, str1, 32);
+	write(STDOUT_FILENO, str2, 16);
+	write(STDOUT_FILENO, str3, 12);
+
 	return (1);
 }
