cf3b9c3de4c6e1f18019b10dee544605ece43eb3
jeffmakuto <hjeffmakuto@gmail.com>
2023-03-17 15:10:36 +0000

    102-print combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,9 +14,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 98; i++)
+	for (i = 0; i < 99; i++)
 	{
-		for (j = i; j < 99; j++)
+		for (j = i + 1; j < 100; j++)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
@@ -26,11 +26,12 @@ int main(void)
 
 			if (i == 98 && j == 99)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar('\n');
+				break;
 			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
-	putchar('\n');
 	return (0);
 }
