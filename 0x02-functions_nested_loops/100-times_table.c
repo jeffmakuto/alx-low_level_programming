fdbeb684290fb5a28fbced3dfdabacc3676c60bc
jeffmakuto <hjeffmakuto@gmail.com>
2023-03-21 16:08:10 +0000

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,46 +1,33 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_times_table - print the @n times table.
  * @n: must be in range 0 to 15
- * * Return: void
+ * Return: void
  */
 void print_times_table(int n)
 {
-	int i, j, max, width;
+	int i, j;
 
 	if (n < 0 || n > 15)
-	{
 		return;
-	}
-
-	max = n * n;
-	width = 1;
-
-	/*determine maximum width of each column*/
-	while (max > 0)
-	{
-		max /= 10;
-		width++;
-	}
 
 	for (i = 0; i <= n; i++)
 	{
-		for (j = i; j <= n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			int v = i * j;
 
 			if (j == 0)
-			{
-				printf("%*d", width, v);
-			}
+				printf("0");
 			else
-			{
-				printf(", %*d", width, v);
-			}
-		}
+				printf("%4d", v);
 
+			if (j < n)
+				printf(",");
+		}
 		printf("\n");
 	}
+
 }
