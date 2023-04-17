1d584c3502876e88004d6d334cb7201b323e0b8b
jeffmakuto <hjeffmakuto@gmail.com>
2023-04-17 14:12:58 +0000

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,17 +2,25 @@
 #define DOG_H
 
 /**
- * struct dog - defines a new type struct dog
- * @name: first element
- * @age: second element
- * @owner: third element
+ * struct dog - a struct representing a dog
+ *
+ * @name: the name of the dog
+ *
+ * @age: the age of the dog
+ *
+ * @owner: the owner of the dog
+ *
+ * Description: This struct represents a dog and its attributes.
  */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-
 };
 
+/* Function prototype for initializing a dog */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif /* DOG_H */
+
