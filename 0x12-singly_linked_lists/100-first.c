4b38254cb8f9d6c84ef46bd8a4a9fee5123d8294
jeffmakuto <hjeffmakuto@gmail.com>
2023-04-27 14:07:20 +0000

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,6 @@
  */
 void __attribute__((constructor)) before_main(void)
 {
-	printf("You're beat! and yet, you must allow, \n");
-	printf("I bore my house upon my back!\n");
+	puts("You're beat! and yet, you must allow,");
+	puts("I bore my house upon my back!");
 }
-
