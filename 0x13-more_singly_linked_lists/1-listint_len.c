07a077eadad0634d8d2695bf3857d85e4ae885ab
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2023-05-02 19:49:43 +0300

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * listint_len - Returns the number of elements in a linked listint_t list
+ * listint_len - Returns the number of elements in a linked list
  
  * @h: Pointer to the 1st node of the linked list
  *
@@ -12,13 +12,13 @@ size_t print_listint(const listint_t *h)
 	size_t count = 0;
 	const listint_t *current = h;
 
-	while (current != NULL)
+	while (current)
 	{
 		printf("%d\n", current->n);
 		count++;
 		current = current->next;
 	}
 
-	return count;
+	return (count);
 }
 
