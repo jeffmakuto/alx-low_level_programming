29acb16f7a53fa74b420bf5eecb51e8744341fe4
jeffmakuto <hjeffmakuto@gmail.com>
2023-05-02 16:52:52 +0000

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,18 +7,17 @@
  *
  * Return: The number of elements in the list
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 	const listint_t *current = h;
 
-	while (current)
+	while (current != NULL)
 	{
-		printf("%d\n", current->n);
 		count++;
 		current = current->next;
 	}
 
-	return (count);
+	return count;
 }
 
