d9e5a632f56f174ec3fa03bdb7d67dd44af054de
jeffmakuto <hjeffmakuto@gmail.com>
2023-05-02 13:28:06 +0000

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,43 +9,43 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t i, counter = 0, size = 5;
+	size_t i, count = 0, size = 1;
 	void **pointers;
 
 	if (!head)
 		return (0);
 
-	/* init pointers list */
-	pointers = calloc(size, sizeof(void *));
+	pointers = malloc(size * sizeof(void *));
 	if (!pointers)
 		exit(98);
 
 	while (head)
 	{
-		/* check if current node is not printed before */
-		for (i = 0; i < counter; i++)
+		for (i = 0; i < count; i++)
 			if (pointers[i] == (void *)head)
 				goto loop_detected;
 
 		printf("[%p] %d\n", (void *)head, head->n);
 
-		/* push the current node pointer to the pointers list */
-		pointers[counter++] = (void *)head;
+		pointers[count++] = (void *)head;
 		head = head->next;
 
-		/* if pointers list is full then reallocate larger memory */
-		if (counter >= size)
+		if (count >= size)
 		{
-			size += 5;
+			size *= 2;
 			pointers = realloc(pointers, size * sizeof(void *));
 			if (!pointers)
 				exit(98);
 		}
 	}
+
 	goto complete;
+
 loop_detected:
 	printf("-> [%p] %d\n", (void *)head, head->n);
+
 complete:
 	free(pointers);
-	return (counter);
+	return (count);
 }
+
