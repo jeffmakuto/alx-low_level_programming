72187b36579d201a284215b2edcc48cf6eaab3ea
jeffmakuto <hjeffmakuto@gmail.com>
2023-05-08 10:51:54 +0000

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,7 @@
  * @error_message: pointer to the error message
  *
  */
-void print_error_and_exit(int exit_code, const char* error_message)
+void print_error_and_exit(int exit_code, const char *error_message)
 {
 	dprintf(STDERR_FILENO, "%s\n", error_message);
 	exit(exit_code);
@@ -28,6 +28,8 @@ int main(int argc, char **argv)
 	int fd_from, fd_to;
 	ssize_t bytes_read, bytes_written;
 	char buffer[BUFFER_SIZE];
+	mode_t mode = O_WRONLY | O_CREAT | O_TRUNC;
+	mode_t permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc != 3)
 		print_error_and_exit(97, "Usage: cp file_from file_to");
@@ -37,14 +39,16 @@ int main(int argc, char **argv)
 	if (fd_from == -1)
 	{
 		char error_message[256];
+
 		sprintf(error_message, "Error: Can't read from file %s", argv[1]);
 		print_error_and_exit(98, error_message);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fd_to = open(argv[2], mode, permissions);
 
 	if (fd_to == -1)
 	{
 		char error_message[256];
+
 		sprintf(error_message, "Error: Can't write to %s", argv[2]);
 		print_error_and_exit(99, error_message);
 	}
@@ -54,11 +58,10 @@ int main(int argc, char **argv)
 		if (bytes_written != bytes_read)
 			print_error_and_exit(99, "Error: Incomplete write operation");
 	}
-
 	if (bytes_read == -1)
 		print_error_and_exit(98, "Error: Failed to read from file");
 	if (close(fd_from) == -1 || close(fd_to) == -1)
 		print_error_and_exit(100, "Error: Can't close file descriptor");
-	return 0;
+	return (0);
 }
 
