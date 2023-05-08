b8602d52c290512731b284108f2df4affc7f81b9
jeffmakuto <hjeffmakuto@gmail.com>
2023-05-08 18:53:35 +0000

    100-elf_header.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,21 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#define USAGE_STATUS 97
+#define READ_FAIL_STATUS 98
+#define WRITE_FAIL_STATUS 99
+#define CLOSE_FAIL_STATUS 100
 #define BUFFER_SIZE 1024
+#define WFILE_MODE 0664
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <elf.h>
+#include <stdarg.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void print_error_and_exit(int exit_code, const char *error_message);
-void print_error(const char *msg);
-void print_header(const Elf64_Ehdr *ehdr);
 
 #endif /* MAIN_H */
