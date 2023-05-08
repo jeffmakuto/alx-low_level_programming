b1e212b4ad5f2b82af10fff342d458bf05576704
jeffmakuto <hjeffmakuto@gmail.com>
2023-05-08 19:04:22 +0000

    Updates

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -20,5 +20,17 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void close_all(int count, ...);
+void copy_content(int fd_dest, int fd_src, char *file_src, char *file_dest);
+void print_title(const char *title);
+void print_header_hex(Elf64_Ehdr *header);
+void print_version(Elf64_Ehdr *header);
+void print_class(Elf64_Ehdr *header);
+void print_data(Elf64_Ehdr *header);
+void print_os_abi(Elf64_Ehdr *header);
+void print_abi_version(Elf64_Ehdr *header);
+void print_type(Elf64_Ehdr *header);
+void print_entry(Elf64_Ehdr *header);
+void print_header(Elf64_Ehdr *header);
 
 #endif /* MAIN_H */
