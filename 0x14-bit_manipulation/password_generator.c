e31cc4fe1fa3df4ede49565272a932d9f8bdc5ea
jeffmakuto <hjeffmakuto@proton.me>
2023-07-06 11:19:42 -0400

    password_generator.c

--- 0x14-bit_manipulation/password_generator.c
+++ 0x14-bit_manipulation/password_generator.c
@@ -1,7 +1,45 @@
 #include "main.h"
 
 /**
- * generatePassword - Generate a random password.
+ * main - Generate the password for crackme3 and write it to `101-password`.
+ *
+ * This program generates a password using a specific pattern and saves it
+ * to a file named "101-password".
+ *
+ * Return: 0 on success, otherwise 1.
+ */
+int main(void)
+{
+	FILE *fd;
+	char password[PASSWORD_LENGTH + 1];
+
+	generatePassword(password);
+	fd = fopen("101-password", "w");
+
+	if (!fd)
+	{
+		perror("Error opening file '101-password'");
+		return (1);
+	}
+
+	if (fprintf(fd, "%s", password) < 0)
+	{
+		perror("Error writing to file");
+		fclose(fd);
+		return (1);
+	}
+
+	if (fclose(fd) == EOF)
+	{
+		perror("Error closing file");
+		return (1);
+	}
+
+	return (0);
+}
+
+/**
+ * generatePassword - Generate a password using a specific pattern.
  *
  * @password: Pointer to the character array where the password will be stored.
  *
@@ -9,50 +47,10 @@
  */
 void generatePassword(char *password)
 {
-	unsigned int mask = 0, randomBit;
 	int i;
 
-	mask |= (1 << 1);   /* Include lowercase letters */
-	mask |= (1 << 5);   /* Include uppercase letters */
-	mask |= (1 << 10);  /* Include digits */
-
 	for (i = 0; i < PASSWORD_LENGTH; i++)
-	{
-		randomBit = rand() % 32; /* Generate a random bit position (0-31) */
-		password[i] = 'A' + (randomBit & mask);
-	}
-
+		password[i] = 0x46c6f48U >> ((i & 3) << 3) & 0xff;
 	password[PASSWORD_LENGTH] = '\0';
-}
-
-/**
- * main - Entry point of the program.
- *
- * This function generates a random password and saves it to a file
- * named "101-password".
- *
- * Return: 0 on success, otherwise 1.
- */
-int main(void)
-{
-	char password[PASSWORD_LENGTH + 1];
-	FILE *file;
-
-	srand(time(NULL));  /* Seed the random number generator */
-	generatePassword(password);
-	file = fopen("101-password", "w");
-
-	if (!file)
-	{
-		perror("Unable to open the file");
-		return (1);
-	}
-
-	fprintf(file, "%s", password);
-	fclose(file);
-
-	printf("Password generated and saved to file.\n");
-
-	return (0);
 }
 
