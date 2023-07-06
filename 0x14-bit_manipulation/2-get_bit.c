97964cfacbb15f503c1876827a3e759114d5ca86
jeffmakuto <hjeffmakuto@proton.me>
2023-07-06 09:17:44 -0400

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,11 +12,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    unsigned long int mask = 1UL << index;
+	unsigned long int mask = 1UL << index;
 
-    if (index >= (sizeof(unsigned long int) * 8))
-        return (-1); /* Error: index out of range */
-
-    return ((n & mask) != 0);
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1); /* Error: index out of range */
+	return ((n & mask) != 0);
 }
 
