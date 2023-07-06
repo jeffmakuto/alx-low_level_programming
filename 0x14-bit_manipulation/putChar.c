6ea6932674571ac916b0ae21d2af615a6818a763
jeffmakuto <hjeffmakuto@proton.me>
2023-07-06 05:13:20 -0400

    main.h, README.md, and putChar.c files

---
+++ 0x14-bit_manipulation/putChar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * putChar - Writes a character to the standard output
+ *
+ * @c: The character to be written
+ *
+ * Return: On success, it returns the written character as an unsigned char cast to an int.
+ *         On failure, it returns EOF (usually -1).
+ */
+int putChar(int c)
+{
+	return write(1, &c, 1);
+}
+
