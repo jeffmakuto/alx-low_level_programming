6572d493f18b8aec25b2f61e476002737096fb1a
jeffmakuto <hjeffmakuto@proton.me>
2023-07-10 09:02:18 -0400

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,84 +1,107 @@
 #include "main.h"
 
 /**
- * main - Copies the content of a file to another file.
+ * close_all - closes all opened file streams with error handling
  *
- * @argc: The number of arguments.
+ * @count: the number of passed file descriptors
  *
- * @argv: An array of arguments.
- *
- * Return: 0 on success, or the appropriate exit code on failure.
+ * Return: Void
  */
-int main(int argc, char *argv[])
+void close_all(int count, ...)
 {
-	int fd_from, fd_to;
+	va_list ap;
 
-	if (argc != 3)
+	va_start(ap, count);
+	for (; count > 0; count--)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-		exit(97);
+		int fd = va_arg(ap, int);
+
+		if (close(fd))
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+			exit(CLOSE_FAIL_STATUS);
+		}
 	}
-	fd_from = open(argv[1], O_RDONLY);
-
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
-			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-
-	copyFileContents(fd_from, fd_to);
-
-	return (0);
+	va_end(ap);
 }
 
 /**
- * copyFileContents - Copies the content from one file descriptor to another.
+ * copy_content - copies content from @fd_src to @fd_dest file descriptors
  *
- * @fd_from: The file descriptor to read from.
+ * @fd_dest: the file descriptor of the destination file
  *
- * @fd_to: The file descriptor to write to.
+ * @fd_src: the file descriptor of the source file
  *
- * Return: 0 on success, or the appropriate exit code on failure.
+ * @file_dest: the destination file name
+ *
+ * @file_src: the source file name
+ *
+ * Return: Void
  */
-int copyFileContents(int fd_from, int fd_to)
+void copy_content(int fd_dest, int fd_src, char *file_src, char *file_dest)
 {
-	int bytes_read, bytes_written;
 	char buffer[BUFFER_SIZE];
+	int bytesRead;
 
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-
-		if (bytes_written == -1 || bytes_written != bytes_read)
+	do {
+		memset(buffer, 0, BUFFER_SIZE);
+		bytesRead = read(fd_src, buffer, BUFFER_SIZE);
+		if (bytesRead < 0)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
+			dprintf(STDERR_FILENO, "Can't read from file %s\n", file_src);
+			exit(READ_FAIL_STATUS);
 		}
-	}
-	if (bytes_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-
-	return (0);
+		if (fd_dest < 0 || write(fd_dest, buffer, bytesRead) < 0)
+		{
+			close_all(1, fd_src);
+			dprintf(STDERR_FILENO, "Can't write to %s\n", file_dest);
+			exit(WRITE_FAIL_STATUS);
+		}
+	} while (bytesRead > 0);
 }
 
+/**
+ * main - a program to copy content of file to another one
+ *
+ * @argc: number of arguments
+ *
+ * @argv: passed arguments as cstring array
+ *
+ * Description:
+ * arguments must to be <file_from> <file_to>
+ *
+ * Return: status code 0 on success,
+ * returns 97 on invalid args,
+ * if can not read from <file_from> returns 98
+ */
+int main(int argc, char **argv)
+{
+	char *file_from, *file_to;
+	int fd_from, fd_to;
+
+	/* check args */
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(USAGE_STATUS);
+	}
+	file_from = argv[1];
+	file_to = argv[2];
+
+	/* open src file */
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(READ_FAIL_STATUS);
+	}
+
+	/* open dest file */
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, WFILE_MODE);
+	/* copy content */
+	copy_content(fd_to, fd_from, file_from, file_to);
+
+	/* close and clean */
+	close_all(2, fd_to, fd_from);
+	return (0);
+}
