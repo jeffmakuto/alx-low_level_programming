e4245938804ed7ea7eaf466afa9cf10a1d3f37c4
jeffmakuto <hjeffmakuto@proton.me>
2023-09-08 16:05:04 +0300

    print key-value

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -3,7 +3,7 @@
 /**
  * hash_table_print - prints a hash table
  * @ht: a pointer to the hash table to print
- * Description: Key/value pairs are printed in the order
+ * Description: key/value pairs are printed in the order
  * they appear in the array of the hash table.
  */
 void hash_table_print(const hash_table_t *ht)
